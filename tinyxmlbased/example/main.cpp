130d8641585a5c9ab134fc4ae9aa250bfbe9c4d9
Aleksandr Neborak <sashatref@mail.ru>
2016-12-02 09:33:34 +0200

    1

--- tinyxmlbased/example/main.cpp
+++ tinyxmlbased/example/main.cpp
@@ -19,7 +19,25 @@ int main(int, char **)
 //    someClass.subClassVec.push_back(SubClass());
 //    someClass.subClassVec.push_back(SubClass());
 
-    std::cout << XmlSerializer::save(someClass, "C:/out.xml", "SomeClass");
+    /*std::cout << XmlSerializer::save(someClass, "C:/out.xml", "SomeClass");*/
+
+	XMLDocument *doc = new XMLDocument();
+	
+	XMLElement *el1 = doc->NewElement("root");
+	el1->SetAttribute("attr1", "value1");
+
+	doc->LinkEndChild(el1);
+
+	XMLElement *el2 = doc->NewElement("subroot");
+
+	//el2->SetText("SomeText <b>Text</b> asdas");
+
+	el2->SetValue("SomeText_asdas");
+
+	el1->LinkEndChild(el2);
+
+	doc->SaveFile("C:/out1.xml");
+
 	_getch();
     return 0;
 }
